789394563f4c41b66e07c828ec41206f22f8bd86
Jaiky <jaikysh76@gmail.com>
2025-01-21 20:54:39 +0530

    Lec 03

--- StudentResult.java
+++ StudentResult.java
@@ -4,13 +4,11 @@ public class StudentResult {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Choice the Number for Result \t 1 : Result \t 0 : NoResult");
-        int num1= sc.nextInt();
-        if(num1==1)
-        {
+        int Input;
+
             /*Marks >=90 -> print “This is Good”
             89 >= Marks >= 60 -> print “This is also Good”
-            59 >= Marks >= 0 -> print “This is Good as well”*/
+            59 >= Marks >= 0 -> print “This is Good as well”
             System.out.println("Enter your marks b/w 1 to 100");
             int num2 = sc.nextInt();
 
@@ -24,7 +22,36 @@ public class StudentResult {
                 System.out.println("Incorrect input");
             }
         } else {
+
             System.out.println("Please select b/w 1 or 0");
-        }
-   }
+        }*/
+        int marks;
+            do{
+
+                System.out.println("Choice the Number for Result \t 1 : Result \t 0 : NoResult");
+                Input= sc.nextInt();
+
+                if (Input == 1) {
+                    System.out.println("Enter your marks");
+                    marks=sc.nextInt();
+
+                    if(marks>=90 && marks<=100)
+                        System.out.println("This is good");
+                    else if(marks>=60 && marks<90) {
+                        System.out.println("This is also good");
+                    }else if (marks>=0 && marks<60) {
+                        System.out.println("This is good as well");
+                    }
+                    else if (Input !=0)
+                    {
+                        System.out.println("No Result");
+                    }else {
+                        System.out.println("Incorrect input");
+                    }
+                }else if(Input ==0)
+                {
+                    System.out.println("No Result");
+                }
+            }while(Input != 0);
+    }
 }
